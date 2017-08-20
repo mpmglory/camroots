f797ab7d9d583e7e1510f7ae48216ad9eee9dc5a
platinnson <joyplatini@gmail.com>
2017-08-20 22:14:49 +0100

    premier commit d'envoie

---
+++ assets/js/menus.js
@@ -0,0 +1,18 @@
+$('#header').html('<header id="topNav"><div class="container">\
+                        <button class="btn btn-mobile" data-toggle="collapse" data-target=".nav-main-collapse"><i class="fa fa-bars"></i></button>\
+                        <a class="logo pull-left" href="index.html"><img src="assets/images/demo/brands/7.jpg" alt="" /></a>\
+                        <div class="navbar-collapse pull-right nav-main-collapse collapse submenu-dark"><nav class="nav-main"><ul id="topMain" class="nav nav-pills nav-main">\
+                                        <li class="active"><a href="index.html">ACCUEIL</a></li>\
+                                        <li class="dropdown"><a class="dropdown-toggle" href="#">A PROPOS DE NOUS</a>\
+                                            <ul class="dropdown-menu nav nav-pills nav-main nav-onepage">\
+                                                <li class="dropdown"><a href="presentation.html">Présentation</a></li>\
+								                <li class="dropdown"><a href="organisation.html">organisation</a></li>\
+											    <li class="dropdown"><a href="public-cible.html">public cible</a></li>\
+											 </ul>\
+                                        <li><a href="competences.html" class="uppercase">nos compétences</a></li>\
+                                        <li><a href="porfolio.html">ALBUM PHOTOS</a></li>\
+                                        <li><a href="page-coming-soon-2.html">CARTOGRAPHIE</a></li>\
+                                        <li><a href="contact.html">POUR NOUS JOINDRE</a></li>\
+</ul></nav></div></div></header>'
+);
+
